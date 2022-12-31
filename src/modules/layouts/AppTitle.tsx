610dfd35cfc5ff7fcd7ec42b9fc1a38bc36f6a66
dimamuchalchuk <dimamuchalchuk@gmail.com>
2022-12-31 09:31:35 +0200

    font-family of title app changed

--- src/modules/layouts/AppTitle.tsx
+++ src/modules/layouts/AppTitle.tsx
@@ -5,7 +5,7 @@ import styles from './styleAppLayouts.module.scss'
 const AppTitle=()=>{
    return(
       <Box sx={{ pb: 3 }}>
-         <Typography variant='h5' component='h1' textAlign={'center'} className={styles.title}  >Project "Moon"</Typography>
+         <Typography variant='h5' component='h1' textAlign={'center'} className={styles.title}  >Project Moon</Typography>
       </Box>
    )
 }
