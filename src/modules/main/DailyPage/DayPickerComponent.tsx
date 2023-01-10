847a6b6ce7989875dd8df23d595d48f517e158e6
Dmytro Mykhalchuk <dimamuchalchuk@gmail.com>
2023-01-10 15:34:27 +0200

    corrected styles of datapicker

--- src/modules/main/DailyPage/DayPickerComponent.tsx
+++ src/modules/main/DailyPage/DayPickerComponent.tsx
@@ -5,6 +5,26 @@ import uk from 'date-fns/locale/uk'
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 
+const css = `
+  .my-selected:not([disabled]) { 
+    font-weight: bold; 
+    border: 2px solid #fff;
+  }
+  .my-selected:hover:not([disabled]) { 
+    border-color: yellow;
+    color: #000;
+  }
+  .rdp-day:hover{
+   border-color: yellow;
+   color: #000;
+   font-weight: bold;
+  }
+  .my-today { 
+    font-weight: bold;
+    font-size: 140%; 
+    color: red;
+  }
+`;
 type DayPickerComponentType = {
    selectedDate: Date
    setSelectedDate: (arg1: Date) => void
@@ -16,12 +36,23 @@ export const DayPickerComponent: React.FC<DayPickerComponentType> = ({ selectedD
       selected && setSelectedDate(selected)
    }, [selected])
    return (
-      <DayPicker
-         showOutsideDays fixedWeeks
-         mode="single"
-         selected={selectedDate}
-         onSelect={setSelected}
-         locale={uk}
+      <>
+          <style>{css}</style>
+         <DayPicker
+            showOutsideDays fixedWeeks
+            mode="single"
+            selected={selectedDate}
+            onSelect={setSelected}
+            locale={uk}
+            modifiersClassNames={{
+               selected: 'my-selected',
+               // today: 'my-today'
+             }}
+            styles={{
+               caption: { color: '#fff' }
+          }}
       />
+      </>
+
    );
 }
